73cc814a3cf295e3830bc7428a17fbeeb079c632
Matt Ward <ward.matt@gmail.com>
2013-03-30 17:48:08 +0000

    Display TypeScript file icon in Projects window.

---
+++ src/TypeScriptBinding/RegisterIconsCommand.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
+
+using System;
+using System.Reflection;
+using ICSharpCode.Core;
+using ICSharpCode.SharpDevelop;
+
+namespace ICSharpCode.TypeScriptBinding
+{
+	public class RegisterIconsCommand : AbstractCommand
+	{
+		public override void Run()
+		{
+			Assembly assembly = typeof(RegisterIconsCommand).Assembly;
+			ResourceService.RegisterImages("ICSharpCode.TypeScriptBinding.Resources.ImageResources", assembly);
+		}
+	}
+}
