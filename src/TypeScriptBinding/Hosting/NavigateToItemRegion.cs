8ca5ee8fd0d85453c91a0fdbf25a282b1ba196f7
chrisber <christian@chrisber.net>
2015-03-11 19:17:53 +0100

    Rename to be compatible #5
    CsharpTypeScriptService transpiles ts classes and interfaces to c#
    classes in order to be compatible we can not extend this classes this
    means we need to implement adapters.
    e.g.
    NavigateToItemRegion is an adapter for NavigateToItem.

---
+++ src/TypeScriptBinding/Hosting/NavigateToItemRegion.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using ICSharpCode.NRefactory;
+using ICSharpCode.NRefactory.Editor;
+using ICSharpCode.NRefactory.TypeSystem;
+
+using TypeScriptLanguageService;
+
+namespace ICSharpCode.TypeScriptBinding
+{
+    public class NavigateToItemRegion
+    {
+        NavigateToItem navItem = null;
+
+        public NavigateToItemRegion(NavigateToItem navItem)
+        {
+            this.navItem = navItem;
+        }
+
+        internal int minChar {
+            get { return navItem.textSpan.start; }
+        }
+
+        internal int limChar {
+            get { return minChar + length; }
+        }
+
+        internal int length {
+            get { return navItem.textSpan.length; }
+        }
+
+        public DomRegion ToRegion(IDocument document)
+        {
+            TextLocation start = document.GetLocation(minChar);
+            TextLocation end = document.GetLocation(limChar);
+            return new DomRegion(start, end);
+        }
+
+        public DomRegion ToRegionStartingFromOpeningCurlyBrace(IDocument document)
+        {
+            int startOffset = GetOpeningCurlyBraceOffsetForRegion(document);
+            TextLocation start = document.GetLocation(startOffset);
+            TextLocation end = document.GetLocation(limChar);
+            return new DomRegion(start, end);
+        }
+
+        int GetOpeningCurlyBraceOffsetForRegion(IDocument document)
+        {
+            int offset = minChar;
+            while (offset < limChar) {
+                if (document.GetCharAt(offset) == '{') {
+                    return offset - 1;
+                }
+                ++offset;
+            }
+
+            return minChar;
+        }
+
+        public bool HasContainer()
+        {
+            return !String.IsNullOrEmpty(navItem.containerName);
+        }
+
+        public string GetFullName()
+        {
+            if (HasContainer()) {
+                return String.Format("{0}.{1}", navItem.containerName, navItem.name);
+            }
+            return navItem.name;
+        }
+
+        public string GetContainerParentName()
+        {
+            int dotPosition = navItem.containerName.IndexOf('.');
+            if (dotPosition > 0) {
+                return navItem.containerName.Substring(0, dotPosition);
+            }
+            return null;
+        }
+    }
+}
---
+++ src/TypeScriptBinding/Hosting/NavigationBarItemRegion.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+using ICSharpCode.NRefactory;
+using ICSharpCode.NRefactory.Editor;
+using ICSharpCode.NRefactory.TypeSystem;
+
+using TypeScriptLanguageService;
+
+namespace ICSharpCode.TypeScriptBinding
+{
+    public class NavigationBarItemRegion
+    {
+        NavigationBarItem navItem = null;
+
+        public NavigationBarItemRegion(NavigationBarItem navItem)
+        {
+            this.navItem = navItem;
+        }
+
+        internal int minChar {
+            get {
+                if (HasSpans) {
+                    return navItem.spans[0].start;
+                }
+                return 0;
+            }
+        }
+
+        internal bool HasSpans {
+            get { return (navItem.spans != null) && (navItem.spans.Length > 0); }
+        }
+
+        internal int limChar {
+            get {
+                if (HasSpans) {
+                    TextSpan span = navItem.spans[0];
+                    return span.start + span.length;
+                }
+                return 0;
+            }
+        }
+
+        internal DomRegion ToRegionStartingFromOpeningCurlyBrace(IDocument document)
+        {
+            int startOffset = GetOpeningCurlyBraceOffsetForRegion(document);
+            TextLocation start = document.GetLocation(startOffset);
+            TextLocation end = document.GetLocation(limChar-1);
+            return new DomRegion(start, end);
+        }
+
+        int GetOpeningCurlyBraceOffsetForRegion(IDocument document)
+        {
+            int offset = minChar;
+            while (offset < limChar) {
+                if (document.GetCharAt(offset) == '{') {
+                    return offset - 1;
+                }
+                ++offset;
+            }
+
+            return minChar;
+        }
+
+    }
+}
+
