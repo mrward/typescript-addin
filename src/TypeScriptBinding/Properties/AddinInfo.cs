646d95c374559efae0b5c8ebcfdfd2e62e76bc65
Matt Ward <ward.matt@gmail.com>
2015-03-25 19:37:18 +0000

    Split up addin information into separate parts.
    Move addin info into attributes.
    Move runtime file imports into a separate file.
    Extensions remain in main TypeScriptBinding.addin.xml file.

---
+++ src/TypeScriptBinding/Properties/AddinInfo.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin ("TypeScript",
+	Namespace = "MonoDevelop",
+	Version = "0.4",
+	Category = "Web Development")]
+
+[assembly:AddinName ("TypeScript")]
+[assembly:AddinDescription ("Adds TypeScript support. Updated to use TypeScript 1.4")]
+
+[assembly:AddinDependency ("Core", "5.0")]
+[assembly:AddinDependency ("Ide", "5.0")]
+[assembly:AddinDependency ("SourceEditor2", "5.0")]
+[assembly:AddinDependency ("Refactoring", "5.0")]
