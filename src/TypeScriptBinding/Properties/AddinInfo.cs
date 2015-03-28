6889399a5d280d8e6ad65d974cab96e0eb98e222
Matt Ward <ward.matt@gmail.com>
2015-03-28 15:29:08 +0000

    Use 32 bit binaries instead of 64 bit for Linux.
    Rename the addin so both can exist in MonoDevelop's addin repository.

--- src/TypeScriptBinding/Properties/AddinInfo.cs
+++ src/TypeScriptBinding/Properties/AddinInfo.cs
@@ -3,13 +3,13 @@ using System;
 using Mono.Addins;
 using Mono.Addins.Description;
 
-[assembly:Addin ("TypeScript",
+[assembly:Addin ("TypeScript.Linux.32bit",
 	Namespace = "MonoDevelop",
 	Version = "0.5",
 	Category = "Web Development")]
 
-[assembly:AddinName ("TypeScript")]
-[assembly:AddinDescription ("Adds TypeScript support. Updated to use TypeScript 1.4")]
+[assembly:AddinName ("TypeScript Linux 32 bit")]
+[assembly:AddinDescription ("Adds TypeScript support for Linux 32 bit. Updated to use TypeScript 1.4")]
 
 [assembly:AddinDependency ("Core", "5.0")]
 [assembly:AddinDependency ("Ide", "5.0")]
