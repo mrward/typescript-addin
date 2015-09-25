66a0c8eaef7557e9e040be31fa38b41bdf5b19c1
Matt Ward <ward.matt@gmail.com>
2015-09-25 17:33:23 +0100

    Update assembly version to 0.7

--- src/TypeScriptBinding/Properties/AddinInfo.cs
+++ src/TypeScriptBinding/Properties/AddinInfo.cs
@@ -5,11 +5,11 @@ using Mono.Addins.Description;
 
 [assembly:Addin ("TypeScript",
 	Namespace = "MonoDevelop",
-	Version = "0.6",
+	Version = "0.7",
 	Category = "Web Development")]
 
 [assembly:AddinName ("TypeScript")]
-[assembly:AddinDescription ("Adds TypeScript support. Updated to use TypeScript 1.5.3")]
+[assembly:AddinDescription ("Adds TypeScript support. Updated to use TypeScript 1.6.2")]
 
 [assembly:AddinDependency ("Core", "5.0")]
 [assembly:AddinDependency ("Ide", "5.0")]
